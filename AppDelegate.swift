81210803b21c3008c437648ff09c4725c032b39c
Abdallah Muftah <abdomuftah@hotmail.com>
2018-10-27 15:00:09 +0400

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,28 +1,29 @@
 //
 //  AppDelegate.swift
-//  Netnotifi
+//  firbaseMessaging
 //
-//  Created by Naruto on 11/19/17.
-//  Copyright © 2017 Naruto. All rights reserved.
+//  Created by ScarNaruto on 10/27/18.
+//  Copyright © 2018 ScarNaruto. All rights reserved.
 //
 
 import UIKit
 import Firebase
+import UserNotifications
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
     
     var window: UIWindow?
     
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (isGranted, err) in
             if err != nil {
                 //Something bad happend
             } else {
-                UNUserNotificationCenter.current().delegate = self
+                UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
                 Messaging.messaging().delegate = self
                 
                 DispatchQueue.main.async {
@@ -31,46 +32,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
             }
         }
         
+        
         FirebaseApp.configure()
         
         return true
-        
     }
-
+    
     
     
     func ConnectToFCM() {
         Messaging.messaging().shouldEstablishDirectChannel = true
         
-        if let token = InstanceID.instanceID().token() {
-            print("DCS: " + token)
+        
+        
+        InstanceID.instanceID().instanceID { (result, error) in
+            if let error = error {
+                print("Error fetching remote instange ID: \(error)")
+            } else if let result = result {
+                print("Remote instance ID token: \(result.token)")
+            }
         }
         
+        
     }
-
+    
     
     func applicationWillResignActive(_ application: UIApplication) {
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         
-        Messaging.messaging().shouldEstablishDirectChannel = false
+        Messaging.messaging().shouldEstablishDirectChannel = true
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         Messaging.messaging().shouldEstablishDirectChannel = false
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         ConnectToFCM()
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         Messaging.messaging().shouldEstablishDirectChannel = false
     }
-
-    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
         ConnectToFCM()
     }
+    
     
 }
